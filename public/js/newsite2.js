30f1084a17f94673d5195588ab0fc67acf5c8b72
Wei Lai <wlai@liveperson.com>
2022-11-12 13:38:57 -0500

    checkpoint almost done

--- public/js/newsite2.js
+++ public/js/newsite2.js
@@ -1,14 +1,260 @@
 let newSite = {};
+async function getConnectorId(account, bearer) {
+  var myHeaders = new Headers();
+  myHeaders.append("authorization", "Bearer " + bearer);
+  myHeaders.append("Cookie", "JSESSIONID=765FED3E0C32AC23D6429203CB5FBDE0");
 
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  return await fetch(
+    "https://va.ac.liveperson.net/api/account/" +
+      account +
+      "/configuration/le-connectors/connectors?v=2.0",
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => {
+      console.log(result);
+      var jsonData = JSON.parse(result);
+      var result = jsonData[0].id;
+      console.log("🚀 connectorId " + jsonData[0].id);
+      return result;
+    })
+    .catch((error) => console.log("error", error));
+}
+
+async function getVisitorBehavior(account, bearer) {
+  var myHeaders = new Headers();
+  myHeaders.append("authorization", "Bearer " + bearer);
+  myHeaders.append("Cookie", "JSESSIONID=F28691A1DE7C425D38B6F49069680962");
+
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  return await fetch(
+    "https://va.ac.liveperson.net/api/account/" +
+      account +
+      "/configuration/le-targeting/visitor-behaviors?v=2.0",
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => {
+      console.log(result);
+      var jsonData = JSON.parse(result);
+      var result = jsonData[0].id;
+      console.log("🚀 visitor behavior " + jsonData[0].id);
+      return result;
+    })
+    .catch((error) => console.log("error", error));
+}
+async function getOnsiteLocationId(account, bearer) {
+  var myHeaders = new Headers();
+  myHeaders.append("authorization", "Bearer " + bearer);
+  myHeaders.append("Cookie", "JSESSIONID=F28691A1DE7C425D38B6F49069680962");
+
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  return await fetch(
+    "https://va.ac.liveperson.net/api/account/" +
+      account +
+      "/configuration/le-targeting/onsite-locations?v=3.0",
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => {
+      console.log(result);
+      var jsonData = JSON.parse(result);
+      var result = jsonData[0].id;
+      console.log("🚀 onsiteLocation " + jsonData[0].id);
+      return result;
+    })
+    .catch((error) => console.log("error", error));
+}
+async function getWindowId(account, bearer) {
+  var myHeaders = new Headers();
+  myHeaders.append("authorization", " Bearer " + bearer);
+  myHeaders.append("Cookie", "JSESSIONID=7702259C698954A664CB0F0E075C0D6B");
+
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  return await fetch(
+    "https://va.ac.liveperson.net/api/account/" +
+      account +
+      "/configuration/engagement-window/window-confs?v=2.0",
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => {
+      console.log(result);
+      var jsonData = JSON.parse(result);
+      var result = jsonData[0].id;
+      console.log("🚀 windowId " + jsonData[0].id);
+      return result;
+    })
+    .catch((error) => console.log("error", error));
+}
+
+async function getCampaigns(account, bearer) {
+  var myHeaders = new Headers();
+  myHeaders.append("authorization", "Bearer " + bearer);
+  myHeaders.append("Cookie", "JSESSIONID=91382CDBABFA988783DEE81B9361B1C0");
+
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  return await fetch(
+    "https://va.ac.liveperson.net/api/account/" +
+      account +
+      "/configuration/le-campaigns/campaigns?v=3.4&fields=id",
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => {
+      console.log(result);
+      var jsonData = JSON.parse(result);
+      var result = jsonData[0].id;
+      console.log("🚀 campaign id " + result);
+      return result;
+    })
+    .catch((error) => console.log("error", error));
+}
+async function setIdp(site) {
+  var myHeaders = new Headers();
+  myHeaders.append("content-type", "application/json");
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: JSON.stringify(site),
+    redirect: "follow",
+  };
+  return await fetch("/newidp", requestOptions)
+    .then((response) => response.status)
+    .then((result) => {
+      console.log(result);
+      return result;
+    })
+    .catch((error) => console.log("error", error));
+}
+
+async function getBearer(site) {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  var raw = JSON.stringify({
+    username: site.userId,
+    password: site.pwd,
+  });
+
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  return await fetch(
+    "https://" + site.vep + "/api/account/" + site.account + "/login?v=1.3",
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => {
+      console.log(result);
+      var jsonData = JSON.parse(result);
+      var result = jsonData.bearer;
+      console.log("🚀 bearer = " + result);
+      return result;
+    })
+    .catch((error) => console.log("error", error));
+}
+
+async function getVep(account) {
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  return await fetch(
+    "https://api.liveperson.net/api/account/" +
+      account +
+      "/service/baseURI?version=1.0",
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => {
+      console.log(result);
+      var jsonData = JSON.parse(result);
+      var baseURIs = jsonData.baseURIs;
+      var result = baseURIs.find(
+        (entry) => entry.service == "agentVep"
+      ).baseURI;
+      console.log("🚀 vep = " + result);
+      return result;
+    })
+    .catch((error) => console.log("error", error));
+}
+async function setEngagement(site) {
+  var myHeaders = new Headers();
+  myHeaders.append("content-type", "application/json");
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: JSON.stringify(site),
+    redirect: "follow",
+  };
+  return await fetch("/newengagement", requestOptions)
+    .then((response) => response.status)
+    .then((result) => {
+      console.log(result);
+      return result;
+    })
+    .catch((error) => console.log("error", error));
+}
 async function createEnagement() {
-  
+  newSite.account = document.getElementById("siteId").value;
+  newSite.userId = document.getElementById("userId").value;
+  newSite.pwd = document.getElementById("pwd").value;
+
+  newSite.vep = await getVep(newSite.account);
+  newSite.bearer = await getBearer(newSite);
+  // newSite.implicit = await setIdp(newSite);
+  newSite.campaign = await getCampaigns(newSite.account, newSite.bearer);
+  newSite.windowId = await getWindowId(newSite.account, newSite.bearer);
+  newSite.onsiteLocaitonId = await getOnsiteLocationId(
+    newSite.account,
+    newSite.bearer
+  );
+  newSite.visitorBehavior = await getVisitorBehavior(
+    newSite.account,
+    newSite.bearer
+  );
+  newSite.connectorId = await getConnectorId(newSite.account, newSite.bearer);
+  newSite.url = await setEngagement(newSite);
+  location.href = "http://localhost:3000/?site=" + newSite.account;
 }
 
 // (A) SHOW & HIDE SPINNER
-function show () {
+function show() {
   // validate form first
   const form = document.querySelector("form");
-  if(!form.checkValidity()){
+  if (!form.checkValidity()) {
     form.classList.add("was-validated");
     return;
   }
@@ -16,11 +262,12 @@ function show () {
   document.getElementById("spinner").classList.add("show");
 
   // perform all actions to do engagement creation
+  createEnagement();
 }
-function hide () {
+function hide() {
   document.getElementById("spinner").classList.remove("show");
 }
 
-window.addEventListener("load", function() {
-  document.getElementById("create").addEventListener("click", show)
-})
+window.addEventListener("load", function () {
+  document.getElementById("create").addEventListener("click", show);
+});
