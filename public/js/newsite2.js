3a486989e88bd3e1c635d45e4e7bb1b39c733531
Wei Lai <wlai@liveperson.com>
2022-11-12 12:09:08 -0500

    validate form

--- public/js/newsite2.js
+++ public/js/newsite2.js
@@ -1,6 +1,15 @@
 // (A) SHOW & HIDE SPINNER
 function show () {
+  // validate form first
+  const form = document.querySelector("form");
+  if(!form.checkValidity()){
+    form.classList.add("was-validated");
+    return;
+  }
+  // if form is good then throw the spinner
   document.getElementById("spinner").classList.add("show");
+
+  // perform all actions to do engagement creation
 }
 function hide () {
   document.getElementById("spinner").classList.remove("show");
